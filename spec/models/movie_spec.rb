dc8a60cb30f5325b62661a84b066d05cf7609ab6
kellyeryan <kellyeryan@gmail.com>
2019-10-04 13:47:58 -0400

    Finish active record crud lab

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -104,7 +104,7 @@ describe 'Movie' do
       end
 
       it 'can get size of the database' do
-        movies_size = can_get_length_of_movie_table
+        movies_size = can_get_length_of_the_movie_table
         expect(movies_size).to eq(5)
       end
 
